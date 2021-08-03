1d77c52453c396f4412a048ca76734fa88ff7f35
datanapps <datanapps@gmail.com>
2021-08-03 12:29:33 +0800

    added context utils

--- app/src/main/java/datanapps/androidutility/utils/extension/ContextUtils.kt
+++ app/src/main/java/datanapps/androidutility/utils/extension/ContextUtils.kt
@@ -1,23 +1,52 @@
 package datanapps.androidutility.utils.extension
 
+import android.annotation.SuppressLint
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.content.Context.CONNECTIVITY_SERVICE
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities.NET_CAPABILITY_INTERNET
+import android.os.Build
+import androidx.core.content.ContextCompat
+
 
 /*
  * create by yogendrapratapsingh
  * on 2/8/21
  */
 
-const val SPACE = " "
-const val EMPTY = ""
-const  val LF = "\n"
-const  val CR = "\r"
-const  val CRLF = "\\r\\n"
-const  val NA = "NA"
+/*
+*
+* Copy clip text enter whatever text
+*
+* */
+fun Context.copyToClipboard(text: CharSequence){
+    val clipboard = ContextCompat.getSystemService(this, ClipboardManager::class.java)
+    val clip = ClipData.newPlainText("label",text)
+    clipboard?.setPrimaryClip(clip)
+}
 
-fun String?.isEmpty(): Boolean = this.isNullOrEmpty()
+/*
+*
+* Check internet connection
+*
+* */
+@Suppress("DEPRECATION")
+fun Context.isNetworkAvailable(): Boolean {
+    val cm = getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        val capabilities = cm.getNetworkCapabilities(cm.activeNetwork)
+        capabilities?.hasCapability(NET_CAPABILITY_INTERNET) == true
+    } else {
+        return  cm.activeNetworkInfo?.isConnectedOrConnecting == true
+    }
+}
 
-fun String?.isNotEmpty(): Boolean  = !this.isNullOrEmpty()
+/*
+Check no internet connection
+* */
+fun Context.isNetworkNotAvailable(): Boolean = !isNetworkAvailable()
 
-fun String?.isNull(): Boolean = this.isNullOrEmpty()
 
-fun String?.length(): Int  = if (this.isNullOrEmpty()) 0 else this.length
 
